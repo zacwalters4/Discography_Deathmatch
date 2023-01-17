30260b0618a1f95923a95fb19c6acf2fbb855d0c
Zac Walters <zacwalters4@gmail.com>
2023-01-17 09:05:44 -0700

    Create initial album tile

--- src/BattlePage/BattlePage.tsx
+++ src/BattlePage/BattlePage.tsx
@@ -1,29 +1,44 @@
 import React from 'react'
 import './BattlePage.css'
-import Search from '../SearchForm/SearchForm'
 import Header from '../Header/Header'
 import { useLocation } from "react-router-dom"
 import { formatName } from '../Utilities/Helper'
 import { getAlbums } from '../Utilities/APICalls'
 
 const BattlePage = () => {
-    
+
+    const [battleAlbums, getBattleAlbums] = React.useState([])
     const locationData = useLocation()
     const artistName = formatName(locationData.search)
 
     const searchAlbums = () => {
         getAlbums(artistName)
             .then(data => {
-                const albumResults: [] = data.topalbums.album.splice(0, 10)
+                getBattleAlbums(data.topalbums.album.splice(0, 10))
             })
         
     }
-    searchAlbums()
+    const startMatch = () => {
+        searchAlbums()
+        console.log(battleAlbums)
+    }
 
-    return (
+
+    return ( 
         <div className="battle-page">
             <Header />
-
+            <button 
+                className="start-button"
+                onClick={startMatch}
+            >
+                Start!
+            </button>
+            <div className="battle-box">
+            {(!!battleAlbums.length) && 
+                <div className="album-left">
+                    <img src={battleAlbums[0].image[3]["#text"]}></img>
+                </div>}
+            </div>
         </div>
     )
 }
