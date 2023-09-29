057bf61d133f5f3fd79460d8afed190214a8433a
Zac <zacwalters4@gmail.com>
2023-09-28 22:31:04 -0600

    Format collection page

--- src/Collection/Collection.tsx
+++ src/Collection/Collection.tsx
@@ -2,20 +2,27 @@ import React from 'react'
 import './Collection.css'
 import { topAlbumsArray, Album } from '../Utilities/Interfaces'
 import AlbumCard from '../AlbumCard/AlbumCard'
-
+import trophy from '../Icons/trophy-solid.svg'
 
 const Collection = ({topAlbums}: topAlbumsArray ) => {
 
     return (
         <div className="collection-page">
+            <img
+                className={"collection-trophy"} 
+                src={trophy}
+                alt="trophy"
+            /> 
             {(!topAlbums.length) && 
-                <h1>There's nothing here yet..</h1>
+                <h2>There's nothing here yet..</h2>
             }
-            {topAlbums.map((album: Album, index: number) => {
-                return (
-                    <AlbumCard album={album} key={index}/>
-                )
-            })}
+            <div className="winning-albums">
+                {topAlbums.map((album: Album, index: number) => {
+                    return (
+                        <AlbumCard album={album} key={index}/>
+                    )
+                })}
+            </div>
         </div>
         
     )
